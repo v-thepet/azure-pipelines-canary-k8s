9e488beb190445a4aaf68ce2938907841cda4484
Theano Petersen <v-thepet@microsoft.com>
2024-07-17 16:28:58 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from prometheus_client import start_http_server, Counter
 app = Flask('sampleapp')
 c = Counter('requests', 'Number of requests served, by custom_status', ['custom_status'])
 
-success_rate = 50
+success_rate = 25
 
 @app.route('/')
 def hello():
